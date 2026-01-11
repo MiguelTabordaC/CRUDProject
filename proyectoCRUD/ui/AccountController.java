c6b05253427d1ceba4495191c04e0c56345b8c11
Felipe <luisfelipea40@gmail.com>
2026-01-11 20:09:38 +0100

    Rectificaciones

--- proyectoCRUD/ui/AccountController.java
+++ proyectoCRUD/ui/AccountController.java
@@ -13,9 +13,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -35,10 +38,34 @@ public class AccountController {
     private Button btnDelete;
     @FXML
     private Button btnExit;
+    @FXML
+    private TableColumn tcId;
+    @FXML
+    private TableColumn tcDescription;
+    @FXML
+    private TableColumn tcType;
+    @FXML
+    private TableColumn tcBegingBalance;
+    @FXML
+    private TableColumn tcBalance;
+    @FXML
+    private TableColumn tcCreditLine;
+    @FXML
+    private TableColumn tcDate;
+ 
+    
+          
     private static final Logger LOGGER = Logger.getLogger("projectinterfaces.ui");
     private Stage stage;
     private String password;
-
+    
+    /**
+    * Inicializa la etapa principal de la ventana Iniciar sesión.
+    * Configura la escena, las propiedades del escenario (título, redimensionable), los estados de control iniciales,
+    * y oyentes para cambios de campo y enfoque, y controladores de acciones para botones e hipervínculos.
+    * @param stage La etapa principal de esta ventana.
+    * @param root El nodo raíz del diseño FXML para esta escena..
+    */
     public void init(Stage stage, Parent root) {
 
         //Se crea la escena asociada al grafico de root.
@@ -51,15 +78,28 @@ public class AccountController {
         //Establecer el titulo de la ventana
         stage.setTitle("Account");
         //La ventana no es redimensionable
-        //stage.setResizable(false);
+        stage.setResizable(false);
         //El botón Login esta deshabilitado y el botón Exit esta habilitado.
         //btnLogin.setDisable(true);
         //btnExit.setDisable(false);
         //Asociar eventos a manejadores
         btnExit.setOnAction(this::handleExitOnAction);
+        tcId.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        tcDescription.setCellValueFactory(new PropertyValueFactory<>("DESCRIPTION"));
+        
+        tcType.setCellValueFactory(new PropertyValueFactory<>("TYPE"));
+        tcCreditLine.setCellValueFactory(new PropertyValueFactory<>("CREDITLINE"));
+        tcDate.setCellValueFactory(new PropertyValueFactory<>("DATE"));
+        
         //Mostrar la ventana
         stage.show();
+        //Cerrar la ventana
+         stage.setOnCloseRequest(this::handleClose);
     }
+    /**
+     * 
+     * @param event Manejador del boton exit
+     */
      private void handleExitOnAction(ActionEvent event) {
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
@@ -75,6 +115,10 @@ public class AccountController {
         }
 
     }
+     /**
+      * 
+      * @param event Manejador del cierre de la ventana
+      */
      private void handleClose(WindowEvent event){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
                 "Are you sure you want to go out?",
