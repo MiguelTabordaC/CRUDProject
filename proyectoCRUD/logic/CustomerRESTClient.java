2cb41265ffaf16287f4149acc5a303fe6d8d9770
david <david@2DAM-013>
2026-01-22 13:47:13 +0100

    edit done, create new customer left

--- proyectoCRUD/logic/CustomerRESTClient.java
+++ proyectoCRUD/logic/CustomerRESTClient.java
@@ -6,10 +6,12 @@
 package proyectoCRUD.logic;
 
 import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
-import proyectoCRUD.model.Customer;
 
 /**
  * Jersey REST client generated for REST resource:CustomerFacadeREST
@@ -35,7 +37,7 @@ public class CustomerRESTClient {
         webTarget = client.target(BASE_URI).path("customer");
     }
 
-    public void edit_XML(Object requestEntity, Long id) throws ClientErrorException {
+    public void edit_XML(Object requestEntity, Long id) throws InternalServerErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
@@ -43,7 +45,7 @@ public class CustomerRESTClient {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
     }
 
-    public <T> T findCustomerByEmailPassword_XML(Class<T> responseType, String email, String password) throws ClientErrorException {
+    public <T> T findCustomerByEmailPassword_XML(Class<T> responseType, String email, String password) throws InternalServerErrorException, NotAuthorizedException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("sigin/{0}/{1}", new Object[]{email, password}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
@@ -67,7 +69,7 @@ public class CustomerRESTClient {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
-    public void create_XML(Object requestEntity) throws ClientErrorException {
+    public void create_XML(Object requestEntity) throws InternalServerErrorException, ForbiddenException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
@@ -91,10 +93,6 @@ public class CustomerRESTClient {
 
     public void close() {
         client.close();
-    }
-
-    public Customer[] findAll_XML() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- proyectoCRUD/ui/CrudCustomerController.java
+++ proyectoCRUD/ui/CrudCustomerController.java
@@ -20,9 +20,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
+import javafx.util.converter.LongStringConverter;
 import javax.ws.rs.core.GenericType;
 import proyectoCRUD.logic.CustomerRESTClient;
 import proyectoCRUD.model.Customer;
@@ -36,27 +40,27 @@ public class CrudCustomerController {
     @FXML
     private TableColumn tbID;
     @FXML
-    private TableColumn tbName;
+    private TableColumn<Customer, String> tbName;
     @FXML
-    private TableColumn tbMidInit;
+    private TableColumn<Customer, String> tbMidInit;
     @FXML
-    private TableColumn tbSurname;
+    private TableColumn<Customer, String> tbSurname;
     @FXML
-    private TableColumn tbStreet;
+    private TableColumn<Customer, String> tbStreet;
     @FXML
-    private TableColumn tbCity;
+    private TableColumn<Customer, String> tbCity;
     @FXML
-    private TableColumn tbState;
+    private TableColumn<Customer, String> tbState;
     @FXML
-    private TableColumn tbZip;
+    private TableColumn<Customer, Integer> tbZip;
     @FXML
-    private TableColumn tbEmail;
+    private TableColumn<Customer, String> tbEmail;
     @FXML
-    private TableColumn tbPhone;
+    private TableColumn<Customer, Long> tbPhone;
     @FXML
-    private TableColumn tbPassw;
+    private TableColumn<Customer, String> tbPassw;
     @FXML
-    private TableView tbCustomers;
+    private TableView<Customer> tbCustomers;
     @FXML
     private Button bAdd;
     @FXML
@@ -85,36 +89,6 @@ public class CrudCustomerController {
         
         bDelete.setDisable(true);
         
-        bExit.setOnAction(this::handleBtExitOnAction);
-        bDelete.setOnAction(this::handleBtDeleteOnAction);
-        bRefresh.setOnAction(this::handleBtRefreshOnAction);
-        
-        CustomerStage.setOnCloseRequest(this::handleBtExitOnAction);
-        tbCustomers.getSelectionModel().selectedItemProperty().addListener(this::handleCustomerTableSelectionChanged);
-        
-        tbID.setCellValueFactory(new PropertyValueFactory<>("id"));
-        tbID.setEditable(true);
-        tbName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-        tbName.setEditable(true);
-        tbMidInit.setCellValueFactory(new PropertyValueFactory<>("middleInitial"));
-        tbMidInit.setEditable(true);
-        tbSurname.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-        tbSurname.setEditable(true);
-        tbStreet.setCellValueFactory(new PropertyValueFactory<>("street"));
-        tbStreet.setEditable(true);
-        tbCity.setCellValueFactory(new PropertyValueFactory<>("city"));
-        tbCity.setEditable(true);
-        tbState.setCellValueFactory(new PropertyValueFactory<>("state"));
-        tbState.setEditable(true);
-        tbZip.setCellValueFactory(new PropertyValueFactory<>("zip"));
-        tbZip.setEditable(true);
-        tbEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
-        tbEmail.setEditable(true);
-        tbPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
-        tbPhone.setEditable(true);
-        tbPassw.setCellValueFactory(new PropertyValueFactory<>("password"));
-        tbPassw.setEditable(true);
-        
         GenericType<List<Customer>> customers = new GenericType<List<Customer>>() {};
         
         List<Customer> customerList = clientManager.findAll_XML(customers);
@@ -122,6 +96,308 @@ public class CrudCustomerController {
         ObservableList<Customer> allCustomers = FXCollections.observableArrayList(customerList);
         
         tbCustomers.setItems(allCustomers);
+        
+        tbCustomers.setEditable(true);
+        
+        bExit.setOnAction(this::handleBtExitOnAction);
+        bDelete.setOnAction(this::handleBtDeleteOnAction);
+        bRefresh.setOnAction(this::handleBtRefreshOnAction);
+        bAdd.setOnAction(this::handleBtAddOnAction);
+        
+        CustomerStage.setOnCloseRequest(this::handleBtExitOnAction);
+        tbCustomers.getSelectionModel().selectedItemProperty().addListener(this::handleCustomerTableSelectionChanged);
+        
+        tbID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tbName.setEditable(false);
+        
+        tbName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        tbName.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbName.setEditable(true);
+        tbName.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(
+                                        t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    //2)
+                    if(newValue.trim().isEmpty() || newValue.trim().length()>30 || !newValue.trim().matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){
+                        throw new Exception("Name field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setFirstName(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        tbMidInit.setCellValueFactory(new PropertyValueFactory<>("middleInitial"));
+        tbMidInit.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbMidInit.setEditable(true);
+        tbMidInit.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(newValue.trim().isEmpty() || newValue.trim().length()>1 || !newValue.trim().matches("[A-Z]+")){
+                        throw new Exception("Middle initial field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setMiddleInitial(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+        tbSurname.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        tbSurname.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbSurname.setEditable(true);
+        tbSurname.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(newValue.trim().isEmpty() || newValue.trim().length()>30 || !newValue.trim().matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){
+                        throw new Exception("Surname field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setLastName(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+        tbStreet.setCellValueFactory(new PropertyValueFactory<>("street"));
+        tbStreet.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbStreet.setEditable(true);
+        tbStreet.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(newValue.trim().isEmpty() || newValue.trim().length()>30 || !newValue.trim().matches("[0-9a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){
+                        throw new Exception("City field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setStreet(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+        tbCity.setCellValueFactory(new PropertyValueFactory<>("city"));
+        tbCity.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbCity.setEditable(true);
+        tbCity.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(newValue.trim().isEmpty() || newValue.trim().length()>30 || !newValue.trim().matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){
+                        throw new Exception("City field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setCity(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+        tbState.setCellValueFactory(new PropertyValueFactory<>("state"));
+        tbState.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbState.setEditable(true);
+        tbState.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(newValue.trim().isEmpty() || newValue.trim().length()>30 || !newValue.trim().matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){
+                        throw new Exception("State field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setState(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+        tbZip.setCellValueFactory(new PropertyValueFactory<>("zip"));
+        tbZip.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        tbZip.setEditable(true);
+        tbZip.setOnEditCommit(
+                (CellEditEvent<Customer, Integer> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    Integer newValue = t.getNewValue();
+                    //2)
+                    if(newValue.toString().trim().isEmpty() || newValue.toString().trim().length()>5 || newValue.toString().trim().length()<5 || !newValue.toString().trim().matches("[0-9]+")){
+                        throw new Exception("Zip field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setZip(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+    
+        
+        tbEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tbEmail.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbEmail.setEditable(true);
+        tbEmail.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(!newValue.trim().matches("^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$") || newValue.trim().isEmpty() || newValue.trim().length()>100){
+                        throw new Exception("Email field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setEmail(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+        tbPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        tbPhone.setCellFactory(TextFieldTableCell.forTableColumn(new LongStringConverter()));
+        tbPhone.setEditable(true);
+        tbPhone.setOnEditCommit(
+                (CellEditEvent<Customer, Long> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    Long newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(!newValue.toString().trim().matches("[0-9 +]+") || newValue.toString().trim().length()>15 || newValue.toString().trim().isEmpty()){
+                        throw new Exception("Phone field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setPhone(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        tbPassw.setCellValueFactory(new PropertyValueFactory<>("password"));
+        tbPassw.setCellFactory(TextFieldTableCell.<Customer>forTableColumn());
+        tbPassw.setEditable(true);
+        tbPassw.setOnEditCommit(
+                (CellEditEvent<Customer, String> t) -> {
+                try{
+                    //1)
+                    Customer item = ((Customer) t.getTableView().getItems().get(t.getTablePosition().getRow())); 
+                    String newValue = t.getNewValue();
+                    String oldValue = item.getFirstName();
+                    //2)
+                    if(newValue.trim().isEmpty() || !newValue.trim().matches("^(?=.*[A-Z])(?=.*\\d).{5,30}$")){
+                        throw new Exception("Password field not valid");
+                    }
+                    //3)
+                    clientManager.edit_XML(item, item.getId());
+                    //4)
+                    item.setPassword(newValue);
+                    //5)
+                    tbCustomers.refresh();
+                }  
+                catch(Exception e){
+                    LOGGER.info(e.getMessage());
+                    tbCustomers.refresh();
+                }
+                
+        });
+        
+        
+       
       
     }
     private void handleBtExitOnAction(Event event){
@@ -140,7 +416,7 @@ public class CrudCustomerController {
             event.consume();
         }
         catch(Exception e){
-            
+            LOGGER.info(e.getMessage());
         }
     }
     
@@ -154,7 +430,7 @@ public class CrudCustomerController {
             }
         }
         catch (Exception e){
-            
+            LOGGER.info(e.getMessage());
         }
     }
     
@@ -173,7 +449,7 @@ public class CrudCustomerController {
             
         }
         catch(Exception e){
-            
+            LOGGER.info(e.getMessage());
         }
         
     }
@@ -182,8 +458,22 @@ public class CrudCustomerController {
             tbCustomers.refresh();
         }
         catch (Exception e){
-            
+            LOGGER.info(e.getMessage());
+        }
+    }
+    
+    private void handleBtAddOnAction(Event event){
+        try{
+            /*
+            Customer customer = new Customer();
+            clientManager.create_XML(customer);
+            tbCustomers.getItems().add(customer);
+            */
+        }
+        catch(Exception e){
+            LOGGER.info(e.getMessage());
         }
     }
         
 }
+
